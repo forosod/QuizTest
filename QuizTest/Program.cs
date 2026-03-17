5d1d7862e38737f161d311a1170b107b786ddc23
Felix Hallberg <fhallberg3@gmail.com>
2026-03-17 14:33:39 +0100

    Added API

--- QuizTest/Program.cs
+++ QuizTest/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, Felix!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+
+        string url = "https://opentdb.com/api.php?amount=1";
+
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine($"Fel: {response.StatusCode}");
+        }
+    }
+}
